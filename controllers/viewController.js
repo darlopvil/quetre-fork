darlopvil/quetre-fork

commit 8a3bd6b4a7f9afeb49f4551224255a279d2ebf0b

refactor: split view logic into many standalone pieces
split the code so as to make at least the answers part reusable.
also added grouped the code in new directories.

controllers/viewController.js
@@ -39,7 +39,9 @@ export const answers = catchAsyncErrors(async (req, res, next) => {
   if (nonSlugRoutes.includes(slug)) return next();
 
   const answersData = await getAnswers(slug);
-  const title = answersData.question.text.spans.map(span => span.text).join('');
+  const title = answersData.question.text.section[0].spans
+    .map(span => span.text)
+    .join('');
 
   res.status(200).render('answers', {
     data: answersData,

app.js
@@ -23,7 +23,6 @@ app.use(
   helmet({
     contentSecurityPolicy: {
       directives: {
-        'img-src': ["'self'"],
         'script-src': ["'self'", 'cdn.jsdelivr.net'],
       },
     },
@@ -33,7 +32,9 @@ app.use(
 
 // 2. SETTING VIEW ENGINE AND PATH TO STATIC ASSETS
 app.set('view engine', 'pug');
-const pathToViews = fileURLToPath(new URL('./views/pug', import.meta.url));
+const pathToViews = fileURLToPath(
+  new URL('./views/pug/pages', import.meta.url)
+);
 app.set('views', pathToViews);
 const pathToPublicDirectory = fileURLToPath(
   new URL('./public', import.meta.url)

fetchers/getAnswers.js
@@ -47,8 +47,8 @@ const getAnswers = async slug => {
         credential: ansObj.node.answer.authorCredential?.translatedString,
         // additionalCredentials: ansObj.node.answer?.credibilityFacts.map(),
       },
-      OriginalQuestion: {
-        text: JSON.parse(ansObj.node.answer.question.title).sections[0],
+      originalQuestion: {
+        text: JSON.parse(ansObj.node.answer.question.title).sections,
         url: ansObj.node.answer.question.url,
         qid: ansObj.node.answer.question.qid,
         isDeleted: ansObj.node.answer.question.isDeleted,
@@ -58,7 +58,7 @@ const getAnswers = async slug => {
   // main data object to be returned
   const data = {
     question: {
-      text: JSON.parse(rawData.title).sections[0],
+      text: JSON.parse(rawData.title).sections,
       url: rawData.url,
       qid: rawData.qid,
       idDeleted: rawData.isDeleted,
@@ -76,7 +76,7 @@ const getAnswers = async slug => {
       questionObj => ({
         qid: questionObj.qid,
         url: questionObj.url,
-        text: JSON.parse(questionObj.title).sections[0],
+        text: JSON.parse(questionObj.title).sections,
       })
     ),
   };

views/pug/base.pug
@@ -40,11 +40,12 @@ html(lang='en')
   body.body
     -if (title !=='About')
       a.skip-link(href="#main") Skip to main content
-    //- stuff that's visible on page goes here
-    include _header
+
+    //- MAIN CONTENT GOES HERE
+    include layout/_header
     block content 
       p placeholder text 
-    include _footer
+    include layout/_footer
 
     //- including mathjax script only when some answer has math expressions(using the var from above)
     if someAnswerContainsMath

views/pug/layout/_answer.pug
@@ -1,33 +1,22 @@
-//- putting spans in semantically correct tags(to the extent possible)
-mixin spansChecker(spans)
-  each span in spans
-    - if(span.modifiers.math) 
-      //- setting that var in the base.pug value to true here.
-      - someAnswerContainsMath = true;
-      span.answer__span-math= `\\(${span.text}\\)`
-    - else if (span.modifiers.code) 
-      code.answer__span-code= span.text
-    - else if (span.modifiers.embed)
-      a.answer__span-link.answers__link(href=span.modifiers.embed.url)= span.modifiers.embed.title || 'link'
-    - else if (span.modifiers.link) //- removing quora.com from the link in case it is a quora.com link.
-      a.answer__span-link.answers__link(href=span.modifiers.link.url.split('https://www.quora.com')[1] || span.modifiers.link.url)=span.text 
-    - else if (!!span.modifiers.bold && !!span.modifiers.italic) //- some people really like to add both bold and emphasis :shrug:
-      strong.answer__span-bold: em.answer__span-italic= span.text
-    - else if (!!span.modifiers.bold)
-      strong.answer__span-bold= span.text
-    - else if (!!span.modifiers.italic)
-      em.answer__span-italic= span.text
-    - else 
-      span.answer__span-plain= span.text 
+//-//////////////////////////////////////////////////////
+//-                 INCLUDES/EXTENDS
+//-//////////////////////////////////////////////////////
+include ../mixins/_formatText.pug
 
+//-//////////////////////////////////////////////////////
+//-                LOCAL HELPER MIXINS
+//-//////////////////////////////////////////////////////
 //- mixin to format date and stuff
 mixin addDate(date)
   - const dateObj = new Date(date / 1000);
     time.answer__metadata-data(datetime= dateObj.toISOString(), title=dateObj.toUTCString())= Intl.DateTimeFormat('en-US', {year: '2-digit', month: 'short', day: 'numeric' }).format(dateObj)
 
-//- putting every answer inside the article tag because it is theorectically independent part of the page
+
+//-//////////////////////////////////////////////////////
+//-                 MAIN CONTENT
+//-//////////////////////////////////////////////////////
 article.answer
-  //- stuff about author
+  //- ABOUT AUTHOR
   figure.answer__author
     figcaption.answer__author-name
       if answerObj.author.isAnon
@@ -41,33 +30,22 @@ article.answer
     img.answer__author-image(src=answerObj.author.avatar.replace('https://', "/api/v1/image/"), alt=`${answerObj.author.name}'s profile photo`, loading='lazy')
     p.answer__author-credentials(aria-label=`${answerObj.author.name}'s credentials`)= answerObj.author.credential || ''
 
+  //- ORIGINAL QUESTION
   h3.answer__question.heading.heading__tertiary
     span Originally answered to&nbsp;
-    a.answer__link.answers__link(href=answerObj.OriginalQuestion.url)
-      +spansChecker(answerObj.OriginalQuestion.text.spans)
+    a.answer__link.answers__link(href=answerObj.originalQuestion.url)
+      +formatText(answerObj.originalQuestion.text)
 
-  //- main answer goes here
+  //- ANSWER
   .answer__text
-    //- putting paragraphs in semantically correct tags(to the extent possible)
-    each para in answerObj.text
-      -if(para.type==='image')
-        img.answer__para.answer__image(src=para.spans[0].modifiers.master_url.replace('https://', "/api/v1/image/"), alt='User embedded image', loading='lazy')
-      - else if (para.type==='code')
-        pre.answer__para.answer__code: code
-          +spansChecker(para.spans)
-      - else if(para.quoted)
-        blockquote.answer__para.answer__quote 
-          +spansChecker(para.spans)
-      - else 
-        p(class=`answer__para answer__${para.type}`)
-          +spansChecker(para.spans)
+    +formatText(answerObj.text)
     //- for quora plus answers. since quora only shows half answer, we gotta warn viewer.
     unless answerObj.isViewable
       p.answer__unviewable
         svg.answer__icon: use(href='/misc/sprite.svg#icon-danger')
         | This is a Quora plus answer and hence full answer is not viewable.
 
-  //- metadata about answer(likes, shares, etc)
+  //- ANSWER METADATA
   .answer__metadata
     p.answer__metadata-item
       svg.answer__icon: use(href='/misc/sprite.svg#icon-clock')

views/pug/layout/_footer.pug
@@ -1,6 +1,12 @@
+//-//////////////////////////////////////////////////////
+//-                   MAIN CONTENT
+//-//////////////////////////////////////////////////////
 footer.footer(class=`${meta.title ==='About' ? 'footer__about' : ''}`)
   block footer
-    //- more stuff will be prepended here on about page
+    
+    //- EXTRA STUFF GOES HERE IF THE PAGE IS ABOUT PAGE
+
+    //- NAVIGATION
     nav.footer__nav-box(aria-label='Primary navigation')
       ul.footer__nav 
         - if (meta.title !=='About') 
@@ -9,7 +15,8 @@ footer.footer(class=`${meta.title ==='About' ? 'footer__about' : ''}`)
         li.footer__nav-item: a.footer__nav-link.footer__link(href="https://github.com/zyachel/quetre") Source Code 
         li.footer__nav-item: a.footer__nav-link.footer__link(href="/privacy") Privacy
         li.footer__nav-item: a.footer__nav-link.footer__link(href="#") Back to top 
-
+    
+    //- LICENSE
     p.footer__license Licensed under&nbsp;
       a.footer__link(href="https://www.gnu.org/licenses/agpl-3.0.html") GNU AGPLv3
       | .

views/pug/layout/_header.pug
@@ -1,13 +1,17 @@
+//-//////////////////////////////////////////////////////
+//-                   MAIN CONTENT
+//-//////////////////////////////////////////////////////
 header.header(class=`${meta.title === 'About' ? 'header__about': ''}`)
+  //- BAR INCLUDES BOTH LOGO AND NAV(WHICH'LL BE THERE ONLY ON ABOUT PAGE)
   .header__bar 
     a.header__link.header__logo(href='/') Quetre
-
     //- for nav on about page
     block header__nav
-
+    
+    //- BUTTON FOR CHANGING THEME
     button.button.theme-changer.header__theme(aria-label='Change Theme')
       svg.icon.icon__theme.theme-changer__icon.theme-changer__icon--sun: use(href='/misc/sprite.svg#icon-sun')
       svg.icon.icon__theme.theme-changer__icon.theme-changer__icon--moon: use(href='/misc/sprite.svg#icon-moon')
 
-  //- for info line on about page
+  //- IF THE PAGE IS ABOUT PAGE, THE BLOCK BELOW WILL GET POPULATED
   block header__info

views/pug/mixins/_formatText.pug
@@ -1,0 +1,54 @@
+//-//////////////////////////////////////////////////////
+//-                LOCAL HELPER MIXINS
+//-//////////////////////////////////////////////////////
+//- this mixin handles a single paragraph, which is passed to it by the main 'formatText' mixin
+mixin spansChecker(spans)
+  each span in spans
+    //- handle tex equations
+    - if(span.modifiers.math)
+      //- setting that var in the base.pug value to true here, so that mathjax library is loaded in the end
+      - someAnswerContainsMath = true;
+      span.answer__span-math= `\\(${span.text}\\)`
+    //- handle small code text
+    - else if (span.modifiers.code) 
+      code.answer__span-code= span.text
+    //- handle embedded content
+    - else if (span.modifiers.embed)
+      a.answer__span-link.answers__link(href=span.modifiers.embed.url)= span.modifiers.embed.title || 'link'
+    //- handle links
+    - else if (span.modifiers.link) //- removing quora.com from the link in case it is a quora.com link.
+      a.answer__span-link.answers__link(href=span.modifiers.link.url.split('https://www.quora.com')[1] || span.modifiers.link.url)=span.text
+    //- handle bold + italic text
+    - else if (!!span.modifiers.bold && !!span.modifiers.italic)
+      strong.answer__span-bold: em.answer__span-italic= span.text
+    //- handle bold text
+    - else if (!!span.modifiers.bold)
+      strong.answer__span-bold= span.text
+    //- handle italic text
+    - else if (!!span.modifiers.italic)
+      em.answer__span-italic= span.text
+    //- regular plain text
+    - else 
+      span.answer__span-plain= span.text 
+
+
+//-//////////////////////////////////////////////////////
+//-           MAIN MIXIN TO FORMAT ANY TEXT
+//-//////////////////////////////////////////////////////
+mixin formatText(text)
+  each para in text
+    //-  handle images
+    -if(para.type==='image')
+      img.answer__para.answer__image(src=para.spans[0].modifiers.master_url.replace('https://', "/api/v1/image/"), alt='User embedded image', loading='lazy')
+    //- handle code blocks
+    - else if (para.type==='code')
+      pre.answer__para.answer__code: code
+        +spansChecker(para.spans)
+    //- handle quotes
+    - else if(para.quoted)
+      blockquote.answer__para.answer__quote 
+        +spansChecker(para.spans)
+    //- rest of the types that don't need/can't be put in a semantic html tag
+    - else 
+      p(class=`answer__para answer__${para.type}`)
+        +spansChecker(para.spans)

commit 4412b726c56f6fe93a91820a758798516e9c908e

fix: fix a fatal bug in viewController.js
this bug was introduced by commit 8a3bd6b4a7f9afeb49f4551224255a279d2ebf0b

controllers/viewController.js
@@ -39,7 +39,7 @@ export const answers = catchAsyncErrors(async (req, res, next) => {
   if (nonSlugRoutes.includes(slug)) return next();
 
   const answersData = await getAnswers(slug);
-  const title = answersData.question.text.section[0].spans
+  const title = answersData.question.text[0].spans
     .map(span => span.text)
     .join('');
 